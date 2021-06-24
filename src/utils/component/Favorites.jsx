67d91eecd4ba2e1f90969851e69e14c729375815
Anna Frumy <81561135+AlphaBlueMoon@users.noreply.github.com>
2021-06-24 22:49:55 +0200

    commit

---
+++ src/utils/component/Favorites.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import network from '../utils/network';
+import Card from './Card';
+
+class Favorites extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			movies: [],
+			favIDs: this.getStorage()
+		};
+	}
+
+	getStorage = () => {
+		return localStorage.getItem('favorites') ? JSON.parse(localStorage.getItem('favorites')) : [];
+	};
+
+	getMovie = async (id) => {
+		const movie = await network.getMovie(id);
+		this.setState({ movies: [ ...this.state.movies, movie ] });
+	};
+
+	componentDidMount() {
+		this.state.favIDs.map((id) => this.getMovie(id));
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Favorites</h1>
+				{this.state.movies.map((movie) => {
+					return (
+						<div key={movie.id} className="col-4 d-flex align-items-stretch p-2">
+							<Card movie={movie} />
+						</div>
+					);
+				})}
+			</div>
+		);
+	}
+}
+
+export default Favorites;
---
+++ src/utils/component/Popular.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import network from '../utils/network';
+import Card from './Card';
+
+export class Popular extends Component {
+	constructor() {
+		super();
+		this.state = {
+			movies: []
+		};
+	}
+
+	async componentDidMount() {
+		const movies = await network.getPopularMovies();
+		this.setState({ movies });
+		console.log(this.state.movies);
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Popular</h1>
+
+				<div className="row">
+					{this.state.movies.map((movie, index) => {
+						return (
+							<div className="col-4 d-flex align-items-stretch p-2">
+								<Card key={index} movie={movie} />
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Popular;
